46a6215c30326a60e981b4427d6f582a3d704ac9
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-09-08 21:12:46 +0800

    finish read_write.py

--- LoadXupt/read_write.py
+++ LoadXupt/read_write.py
@@ -7,17 +7,13 @@ import codecs
 
 def get_name():
 	info = codecs.open('name_list.txt', 'r', 'utf-8')
-	result_name = codecs.open('result.txt', 'w+', 'utf-8')
-
 	for name_info in info:
-	#	write_name(name_info)
-		print name_info.encode('utf-8'),
-		result_name.write(name.decode('utf-8'))
+		print (name_info)
+		write_name(name_info)
+		#write_name("中文输入测试")
 def write_name(word):
-	result_name = codecs.open('result.txt', 'w+', 'utf-8')
-	print word.decode('utf-8')
-	result_name.write(word.decode('utf-8'))
+	result_name = codecs.open('result.txt', 'a', 'utf-8')
+	result_name.write(word)
 
 if __name__ == '__main__':
 	get_name()
-
